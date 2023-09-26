11ddfe68716fc48ade6a72c97772028caa966861
CamposBG <brunogalvaocampos@gmail.com>
2023-09-26 16:54:29 -0300

    commit test

--- sendEmail.js
+++ sendEmail.js
@@ -1,45 +1,49 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
-function translateSubject(subject=subject) {
+// comment here
+function translateSubject(subject = subject) {
   if (subject === "1") {
-    return "refund"
+    return "refund";
   } else if (subject === "2") {
-    return "shipping"
+    return "shipping";
   } else {
-    return "other"
+    return "other";
   }
 }
 
-const sendEmail = async function (FirstName, LastName, email, subject, message) {
+const sendEmail = async function (
+  FirstName,
+  LastName,
+  email,
+  subject,
+  message
+) {
   return new Promise((resolve, reject) => {
     const transporterOptions = {
       service: "outlook",
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAILSECRET,
-      }
-    }
-    const transporter = nodemailer.createTransport(transporterOptions)
+      },
+    };
+    const transporter = nodemailer.createTransport(transporterOptions);
     const sendOptions = {
       from: process.env.EMAIL,
       to: process.env.EMAIL,
       subject: `${FirstName} ${LastName} need help with ${subject}`,
-      text: `${message} FROM ${email}`
+      text: `${message} FROM ${email}`,
     };
-
 
     transporter.sendMail(sendOptions, function (error, info) {
       if (error) {
         console.log("error is ", error);
         resolve(false); // or use rejcet(false) but then you will have to handle errors
-      }
-      else {
+      } else {
         console.log("Email sent:", info);
         resolve(true);
       }
     });
-  })
-}
+  });
+};
 
-module.exports = {translateSubject, sendEmail}
-
+module.exports = { translateSubject, sendEmail };
